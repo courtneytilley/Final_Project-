852c900cccc533b1f251457a88621b10bd327654
courtneytilley <cnt0005@mix.wvu.edu>
2021-03-25 11:02:05 -0700

    courtney tilley data analysis upload

--- Projects/Projects.r
+++ Projects/Projects.r
@@ -1,2 +1,8 @@
 setwd('~/Desktop/Evolution/Projects/Projects')
 Data <- read.csv('data.csv', stringsAsFactors=F)
+setwd('~/Desktop/Evolution/Projects/Projects')
+Data <- read.csv('data2.csv', stringsAsFactors=F)
+Eggs <- which(Data[,1] == "EggSize")
+EggWt <- Data[Eggs,"mass"]
+Latitude <- Data[Eggs,"latitude"]
+plot(abs(Latitude), EggWt, log="y")
